b156c5acb553417fc8a089b02e72bb43bfbd875f
josep rigo <jrrr13@gmail.com>
2020-09-21 17:54:02 +0200

    new ssh

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
