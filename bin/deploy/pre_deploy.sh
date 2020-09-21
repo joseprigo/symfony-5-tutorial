7570b1b80d891578eebf5be8d20cf3ea5631e60b
josep rigo <jrrr13@gmail.com>
2020-09-21 18:08:52 +0200

    remove with Rf parameter

--- bin/deploy/pre_deploy.sh
+++ bin/deploy/pre_deploy.sh
@@ -1,7 +1,7 @@
-# Remove symlink
-sudo rm -R /var/www/micropost_old && \
+sudo rm -Rf /var/www/micropost_old && \
 sudo cp -R /var/www/micropost_current /var/www/micropost_old/ && \
 sudo rm /var/www/micropost && \
+# need to find another solution instead of removing current directory since it deletes all the user uploaded files
 sudo rm -R /var/www/micropost_current && \
 # Create symlink to older version && \
 sudo ln -s /var/www/micropost_old /var/www/micropost
