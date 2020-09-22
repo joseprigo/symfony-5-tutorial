a50be8b97d52a80658049aae39518a04a2cce35f
josep rigo <jrrr13@gmail.com>
2020-09-22 22:44:13 +0200

    copy from udemy

--- src/Repository/NotificationRepository.php
+++ src/Repository/NotificationRepository.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+
 /**
  * @method Notification|null find($id, $lockMode = null, $lockVersion = null)
  * @method Notification|null findOneBy(array $criteria, array $orderBy = null)
