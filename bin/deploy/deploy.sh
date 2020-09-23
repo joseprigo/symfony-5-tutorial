45b2360fb9819c5f6a2ab541716d238db8020f91
josep rigo <jrrr13@gmail.com>
2020-09-23 13:12:11 +0200

    database hyphen in name

--- bin/deploy/deploy.sh
+++ bin/deploy/deploy.sh
@@ -3,7 +3,7 @@ sudo ln -s /var/www/micropost_current /var/www/micropost && \
 cd /var/www/micropost && \
 echo $APP_ENV && \
 echo $DATABASE_URL  &&\
-sudo APP_ENV=$APP_ENV DATABASE_URL=$DATABASE_URL php bin/console doctrine:migrations:migrate --no-interaction && \
+sudo APP_ENV=$APP_ENV DATABASE_URL=mysql://root:root@127.0.0.1/micro-post php bin/console doctrine:migrations:migrate --no-interaction && \
 sudo chown -R www-data:www-data /var/www/micropost_current && \
 sudo chown -h www-data:www-data /var/www/micropost && \
 sudo service apache2 restart
